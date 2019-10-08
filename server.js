2e40cd40b14fd2fb312f7f6758bcfff8d5f7b428
Ibbus93 <ibbus93@gmail.com>
2019-10-08 20:49:01 +0200

    [fix] References

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/studenti'));
+app.use(express.static(__dirname + '/dist/Studenti'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/studenti/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Studenti/index.html'));
 });
 
 // default Heroku PORT
